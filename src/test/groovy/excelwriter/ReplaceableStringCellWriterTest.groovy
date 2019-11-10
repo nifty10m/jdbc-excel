89ac1f9fe97db515b8da3699f22eb8d5a4adb307
Jörg Herbst <joerg.herbst@10m.de>
2019-11-10 19:40:14 +0100

    Added a first unit test

---
+++ src/test/groovy/excelwriter/ReplaceableStringCellWriterTest.groovy
@@ -0,0 +1,16 @@
+package excelwriter
+
+import spock.lang.Specification
+
+
+class ReplaceableStringCellWriterTest extends Specification {
+  def "test replaceAll"() {
+    given:
+      def writer = new ReplaceableStringCellWriter()
+    when:
+      def result = writer.replaceAll("http://{baseurl}/ab/de/{image}", ["baseurl": "google", "image": "cat.png"])
+    then:
+      result == "http://google/ab/de/cat.png"
+  }
+
+}
